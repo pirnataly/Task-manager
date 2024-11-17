f61ee955f9e64cd1600e311320306754abfc2b54
pirnataly <piroznatal@gmail.com>
2024-11-17 23:07:27 +0300

    fix: add correct updating deadline, place, responsible person fields

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -21,13 +21,13 @@ export class TodoItem {
   responsiblePerson: string;
   place: string;
 
-  constructor(heading: string, description: string, data: string, status: Status,deadline:Date,responsible:string,place:string) {
+  constructor(heading: string, description: string, data: string, status: Status, deadline: Date, responsible: string, place: string) {
     this.heading = heading;
     this.description = description;
     this.data = data;
     this.status = status;
     this.deadline = deadline;
-    this.responsiblePerson=responsible;
+    this.responsiblePerson = responsible;
     this.place = place;
   }
 }
@@ -52,7 +52,7 @@ export class Item {
   removeButton: HTMLElement;
 
 
-  constructor(heading: string, description: string, time: string, status: Status = Status.todo,deadline:Date,responsible:string,place:string) {
+  constructor(heading: string, description: string, time: string, status: Status = Status.todo, deadline: Date, responsible: string, place: string) {
     this.itemContainer = createElement('div', 'item-container');
     this.itemHeading = document.createElement('input');
     this.itemHeading.value = heading;
@@ -71,19 +71,24 @@ export class Item {
       this.statusField.setAttribute('checked', 'checked')
     }
     this.extraBlock = createElement('ul', 'extra-block');
-    const extraFields: Extra[] = ['deadline','responsible','place'];
+    const extraFields: Extra[] = ['deadline', 'responsible', 'place'];
     extraFields.forEach(extraField => {
       const li = createElement('li', 'extra-block__li');
       const label = createElement('label', 'extra-field__label');
       label.setAttribute('for', extraField);
       label.textContent = extraField;
       const extraInput = createElement('input', 'extra-field__input') as HTMLInputElement;
-      extraInput.setAttribute('type', extraField==='deadline'?'Date':'text');
+      extraInput.setAttribute('type', extraField === 'deadline' ? 'Date' : 'text');
       extraInput.setAttribute('id', extraField);
-      if(extraField ==='deadline'){
-        extraInput.value = `${deadline.getFullYear()}-${deadline.getMonth()}-${deadline.getDate()}`;
+      if (extraInput.id === 'deadline') {
+         const stringValue = `${deadline.getFullYear()}-${String(deadline.getMonth() + 1).padStart(2,'0')}-${String(deadline.getDate()).padStart(2,'0')}`;
+        extraInput.value = stringValue;
+      } else {
+        extraInput.value = extraField === 'responsible' ? responsible : place;
       }
-
+      if(this.itemHeading.value.trim().length){
+        extraInput.setAttribute('disabled','disabled');
+      }
       li.append(label, extraInput);
       this.extraBlock.append(li);
     })
@@ -98,14 +103,14 @@ export class Item {
     this.saveUpdating.setAttribute('disabled', 'disabled');
     this.removeButton = createElement('button', 'action-button');
     this.removeButton.textContent = 'Remove';
-    if (heading.trim().length !==0) {
+    if (heading.trim().length !== 0) {
       this.itemHeading.setAttribute('disabled', 'disabled');
       this.saveButton.setAttribute('disabled', 'disabled');
-      this.itemDescription.setAttribute('disabled','disabled');
+      this.itemDescription.setAttribute('disabled', 'disabled');
       this.updateButton.removeAttribute('disabled');
     } else {
       this.itemHeading.placeholder = 'Enter the task heading';
-      this.statusField.setAttribute('disabled','disabled');
+      this.statusField.setAttribute('disabled', 'disabled');
     }
     this.itemTime = createElement('p', 'item-time');
     this.itemTime.textContent = time;
@@ -117,8 +122,9 @@ export class Item {
   addEventListeners(time: string) {
     this.statusField.addEventListener('change', () => {
       this.saveUpdate();
+      this.disableInputs()
     });
-    this.itemHeading.addEventListener('input',()=>{
+    this.itemHeading.addEventListener('input', () => {
       this.itemHeading.classList.remove('heading-attention');
     })
 
@@ -130,11 +136,13 @@ export class Item {
         this.statusField.removeAttribute('disabled');
         this.itemHeading.setAttribute('disabled', 'disabled');
         this.itemDescription.setAttribute('disabled', 'disabled');
-        this.extraBlock.setAttribute('disabled','disabled');
+        this.extraBlock.setAttribute('disabled', 'disabled');
+        const extraInputs = this.getInputsFromElement(this.extraBlock);
+        extraInputs.forEach(extraInput => extraInput.setAttribute('disabled', 'disabled'));
         const tempArray = this.getItems();
         const status = this.statusField.value === 'completed' ? Status.completed : Status.todo;
-        const extraBlockInputs = this.extraBlock.getElementsByTagName('input');
-        const newTodoItem = new TodoItem(this.itemHeading.value, this.itemDescription.value, time, status,new Date(extraBlockInputs[0].value),extraBlockInputs[1].value,extraBlockInputs[2].value);
+        const extraBlockInputs = this.getInputsFromElement(this.extraBlock);
+        const newTodoItem = new TodoItem(this.itemHeading.value, this.itemDescription.value, time, status, new Date(extraBlockInputs[0].value), extraBlockInputs[1].value, extraBlockInputs[2].value);
         tempArray.push(newTodoItem);
         localStorage.setItem('items', JSON.stringify(tempArray));
         this.itemHeading.classList.remove('heading-attention');
@@ -143,17 +151,19 @@ export class Item {
         this.itemHeading.placeholder = 'Enter the task heading'
       }
     });
+
     this.updateButton.addEventListener('click', (e) => {
       this.saveUpdating.removeAttribute('disabled');
+      const extraInputs = this.getInputsFromElement(this.extraBlock);
+      extraInputs.forEach(extraInput => extraInput.removeAttribute('disabled'));
+      this.itemHeading.removeAttribute('disabled');
+      this.itemDescription.removeAttribute('disabled');
       this.updateButton.setAttribute('disabled', 'disabled');
     })
 
     this.saveUpdating.addEventListener('click', () => {
       this.saveUpdate();
-      this.saveUpdating.setAttribute('disabled', 'disabled');
-      this.itemHeading.setAttribute('disabled', 'disabled');
-      this.itemDescription.setAttribute('disabled', 'disabled');
-      this.updateButton.removeAttribute('disabled');
+      this.disableInputs();
     })
   }
 
@@ -173,17 +183,22 @@ export class Item {
     }
   }
 
+  getInputsFromElement(elem: HTMLElement) {
+    return Array.from(elem.getElementsByTagName('input'));
+  }
+
   update(todo: TodoItem, part: Partial<TodoItem>) {
     return {...todo, ...part};
   }
 
-  saveUpdate():void {
+  saveUpdate(): void {
     if (this.itemHeading.value.trim().length) {
       const creationTime: string = this.itemTime.textContent as string;
       const tempArray: TodoItem[] = this.getItems();
       const editedIndex = tempArray.findIndex((item) => item.data === creationTime);
       const editedObject = tempArray[editedIndex];
       const extraFieldsInputs = this.extraBlock.getElementsByTagName('input');
+
       const edited = this.update(editedObject, {
         heading: this.itemHeading.value,
         description: this.itemDescription.value,
@@ -199,6 +214,14 @@ export class Item {
       this.itemHeading.placeholder = 'Enter the task heading'
     }
 
+  }
+
+  disableInputs() {
+    this.saveUpdating.setAttribute('disabled', 'disabled');
+    this.itemHeading.setAttribute('disabled', 'disabled');
+    this.itemDescription.setAttribute('disabled', 'disabled');
+    this.updateButton.removeAttribute('disabled');
+    this.getInputsFromElement(this.extraBlock).forEach(inputField => inputField.setAttribute('disabled', 'disabled'));
   }
 
 }
